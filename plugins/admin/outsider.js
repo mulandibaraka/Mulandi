ea775b5045adf3db1f0a85fc43b44fbff780b18e
Mohamed <125765427+Mohalicious@users.noreply.github.com>
2023-03-16 04:43:04 +0300

    Update outsider.js

--- plugins/admin/outsider.js
+++ plugins/admin/outsider.js
@@ -1,5 +1,5 @@
 exports.run = {
-   usage: ['outsider'],
+   usage: ['foreigners'],
    use: '(option)',
    category: 'admin tools',
    async: async (m, {
@@ -10,10 +10,10 @@ exports.run = {
       participants
    }) => {
       try {
-         let member = participants.filter(v => !v.admin).map(v => v.id).filter(v => !v.startsWith('62') && v != client.decodeJid(client.user.id))
+         let member = participants.filter(v => !v.admin).map(v => v.id).filter(v => !v.startsWith('254') && v != client.decodeJid(client.user.id))
          if (!args || !args[0]) {
-            if (member.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 This group is clean from outsiders.`), m)
-            let teks = `✅ *${member.length}* outsiders found, send *${isPrefix + command} -y* to remove them.\n\n`
+            if (member.length == 0) return client.reply(m.chat, Func.texted('bold', ` This group is clean from foreigners.`), m)
+            let teks = `✅ *${member.length}* foreigners found, send *${isPrefix + command} -y* to remove them.\n\n`
             teks += member.map(v => '◦  @' + v.replace(/@.+/, '')).join('\n')
             client.reply(m.chat, teks, m)
          } else if (args[0] == '-y') {
@@ -21,7 +21,7 @@ exports.run = {
                await Func.delay(2000)
                await client.groupParticipantsUpdate(m.chat, [jid], 'remove')
             }
-            await client.reply(m.chat, Func.texted('bold', `🚩 Done, ${member.length} outsiders successfully removed.`), m)
+            await client.reply(m.chat, Func.texted('bold', ` Done, ${member.length} foreigners successfully removed.`), m)
          }
       } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
