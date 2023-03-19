fd662b1e32f213bdf782be752cd8288788f587d4
mulandibaraka <Mulandibaraka@gmail.com>
2023-03-19 17:48:44 +0300

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '254769527061'
+global.owner = '254768827492'
 // Owner name
-global.owner_name = ' Naruto 🗿🚬'
+global.owner_name = '🗿🚬'
 // Maximum upload file size limit (Default : 50 MB)
 global.max_upload = 5000
 // Delay for spamming protection (Default : 3 seconds)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '254769527061@c.us'
+global.forwards = '254768827492@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
